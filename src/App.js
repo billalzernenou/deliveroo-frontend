ebe3ff0be075b4f6edc8486ac12eda590f6378c3
Billal ZERNENOU <billalzernenou@gmail.com>
2021-02-08 17:55:51 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [restaurant, setRestaurent] = useState([]);
+  const [categories, setCategories] = useState([]);
+  // const [restaurants, setRestaurants] = useState([]);
+  // fetch data function
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://the-deliveroo-backend.herokuapp.com/"
+      );
+
+      setRestaurent(response.data.restaurant);
+      setCategories(response.data.categories);
+      console.log(response.data.categories);
+    } catch (error) {
+      alert("An error occurred");
+    }
+  };
+  // fetchData();
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <div style={{ textAlign: "center" }}>{restaurant.name}</div>
+      <div>
+        {
+          //iterate throw catogories
+          categories.map((category, index) => {
+            return (
+              <div>
+                <div key={index}>{category.name}</div>
+                <div style={{ backgroundColor: "grey" }}>
+                  {/* iterate throw each category*/}
+                  {category.meals.map((meal, index) => {
+                    return meal.title;
+                  })}
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   );
-}
+};
 
 export default App;
